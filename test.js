c050ec0be32d67e743710fb7a7c5b49ac3c14c3f
Omar Hamid <omarhamid664@gmail.com>
2023-02-12 21:48:42 -0500

    array loopingusing 'with'

--- test.js
+++ test.js
@@ -1,17 +1,1 @@
-function makeLine(length) {
-  var line = "";
-  for (var j = 1; j <= length; j++) {
-      line += "* ";
-  }
-  return line + "\n";
-}
-
-// your code goes here.  Make sure you call makeLine() in your own code.
-function buildTriangle(height) {
-  for (let i = 1; i <= height; i++) {
-      console.log(makeLine(i));
-  }
-}
-
-// test your code by uncommenting the following line
-buildTriangle(10);
+console.log("Bob" || "Me");
